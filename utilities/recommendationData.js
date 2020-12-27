22272a446aaa36f6cc94102dcba6f39ca54bebde
Talha Iqbal <talhaiqbal801@gamil.com>
2020-12-27 18:39:00 +0500

    Added component for recommended list item

---
+++ utilities/recommendationData.js
@@ -0,0 +1,18 @@
+export default recommendedItems = [
+    {
+        id: jjhid76d87,
+        itemName: 'item 1'
+    },
+    {
+        id: kdf78s,
+        itemName: 'item 2'
+    },
+    {
+        id: jdbhh87a8,
+        itemName: 'item 3'
+    },
+    {
+        id: hjcshjkuy8378,
+        itemName: 'item 4'
+    }
+];
